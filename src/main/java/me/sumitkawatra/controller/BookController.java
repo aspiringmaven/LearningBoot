8f9696531a8cd7d5f9fd4196b097a03217bf4982
Sumit Kawatra <sumitkkawatra@gmail.com>
2017-03-26 01:08:14 +0530

    Book updated call fix.

--- src/main/java/me/sumitkawatra/controller/BookController.java
+++ src/main/java/me/sumitkawatra/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,7 +37,7 @@ public class BookController {
 		bookService.addBook(book);
 	}
 	
-	@PostMapping("/categories/{categoryId}/books/{isbn}")
+	@PutMapping("/categories/{categoryId}/books/{isbn}")
 	public void updateBook(@PathVariable("categoryId") String categoryId, @PathVariable("isbn") String isbn,@RequestBody Book book) {
 		book.setCategory( new Category(categoryId, "", ""));
 		book.setIsbn(isbn);
